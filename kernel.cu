c288ccddfa303478ebc30facf70f2bb32f772d17
neworderofjamie <neworderofjamie@gmail.com>
2021-03-18 18:23:41 +0000

    get rid of fast divide

--- kernel.cu
+++ kernel.cu
@@ -79,11 +79,6 @@ enum class Model
     eProp,
 };
 
-__device__ uint32_t fastDivide(uint32_t x, uint32_t a, uint32_t b, uint32_t m)
-{
-    return (((uint64_t)x * a) + b) >> (32u + m);
-}
-
 __global__ void continuousDense(unsigned int numPre, unsigned int numPost,
                                 float *d_output, float *d_ePre, float *d_g)
 {
@@ -91,18 +86,6 @@ __global__ void continuousDense(unsigned int numPre, unsigned int numPost,
 
     if(id < (numPre * numPost)) {
         atomicAdd(&d_output[id % numPost], d_g[id] * d_ePre[id / numPost]);
-    }
-}
-
-__global__ void continuousDenseFastDivide(unsigned int numSynapse, unsigned int numPost, uint32_t numPostA, uint32_t numPostB, uint32_t numPostM,
-                                          float *d_output, float *d_ePre, float *d_g)
-{
-    const unsigned int id = threadIdx.x + (blockIdx.x * blockDim.x);
-
-    if(id < numSynapse) {
-        const unsigned int pre = fastDivide(id, numPostA, numPostB, numPostM);
-        const unsigned int post = id - (numPost * pre);
-        atomicAdd(&d_output[post], d_g[id] * d_ePre[pre]);
     }
 }
 
@@ -168,26 +151,6 @@ void deviceToHostCopy(HostDeviceArray<T> &array, size_t count)
     CHECK_CUDA_ERRORS(cudaMemcpy(array.first, array.second, count * sizeof(T), cudaMemcpyDeviceToHost));
 }
 //-----------------------------------------------------------------------------
-std::tuple<uint32_t, uint32_t, uint32_t> calcFastDivideConstants(uint32_t d)
-{
-    const uint32_t m = (uint32_t)std::floor(std::log2(d));
-
-    const uint32_t uintMax = std::numeric_limits<uint32_t>::max();
-    if(d == (1 << m)) {
-        return std::make_tuple(uintMax, uintMax, m);
-    }
-    else {
-        const uint32_t t = (1ull << (m + 32)) / d;
-        const uint32_t r = ((t * d) + d) & uintMax;
-        if(r <= (1u << m)) {
-            return std::make_tuple(t + 1, 0, m);
-        }
-        else {
-            return std::make_tuple(t, t, m);
-        }
-    }
-}
-//-----------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
     unsigned int blockSize = 32;
@@ -214,12 +177,6 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "Model:" << static_cast<int>(model) << ", num neurons:" << numNeurons << ", block size:" << blockSize << std::endl;
-
-    // Calculate fast divide constants
-    uint32_t numPostA;
-    uint32_t numPostB;
-    uint32_t numPostM;
-    std::tie(numPostA, numPostB, numPostM) = calcFastDivideConstants(numNeurons);
 
     const unsigned int numSynapses = numNeurons * numNeurons;
 
@@ -257,26 +214,6 @@ int main(int argc, char *argv[])
 
             for(unsigned int i = 0; i < numTimesteps; i++) {
                 continuousDense<<<grid, threads>>>(numNeurons, numNeurons, output.second, ePre.second, g.second);
-            }
-
-            deviceToHostCopy(output, numNeurons);
-            const float sum = std::accumulate(&output.first[0], &output.first[numNeurons], 0.0f);
-            std::cout << "Sum:" << sum << std::endl;
-        }
-
-        // Zero output
-        std::fill_n(&output.first[0], numNeurons, 0.0f);
-        hostToDeviceCopy(output, numNeurons);
-
-        {
-            Timer<std::milli> t("Continuous Dense Fast Divide:");
-            const unsigned int numBlocks = (numSynapses + blockSize - 1) / blockSize;
-            dim3 threads(blockSize, 1);
-            dim3 grid(numBlocks, 1);
-
-            for(unsigned int i = 0; i < numTimesteps; i++) {
-                continuousDenseFastDivide<<<grid, threads>>>(numSynapses, numNeurons, numPostA, numPostB, numPostM,
-                                                             output.second, ePre.second, g.second);
             }
 
             deviceToHostCopy(output, numNeurons);
